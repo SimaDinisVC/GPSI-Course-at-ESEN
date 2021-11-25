d8961c6582ff44ae4e18983bbd5fe0b0bfd15bb8
Simão Costa <simadinisvc@gmail.com>
2021-11-25 22:45:21 +0000

    Update 7º_exercise.py

--- Python/1 - Introduction to Python/7º_exercise.py
+++ Python/1 - Introduction to Python/7º_exercise.py
@@ -1,10 +1,10 @@
 #variaveis
 comp_sala = int(input("Indique o comprimento da sala/divisão:\n"))
-larg_sala = int(input("Indique a largura da sala/divisão:\n"))
+largu_sala = int(input("Indique a largura da sala/divisão:\n"))
 comp_tijoleira = int(input("Indique o comprimento da tijoleira:\n"))
 larg_tijoleira = int(input("Indique a largura da tijuleira:\n"))
 #processamento
-largura = larg_sala/larg_tijoleira
+largura = largu_sala/larg_tijoleira
 comprimento = comp_sala/comp_tijoleira
 resultado = largura * comprimento
 #saida de dados
