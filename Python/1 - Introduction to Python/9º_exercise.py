0e467ad8973a1eaa34f633cce1340cd4692d6c7c
Simão Costa <simadinisvc@gmail.com>
2021-11-25 22:38:24 +0000

    Create 9º_exercise.py

---
+++ Python/1 - Introduction to Python/9º_exercise.py
@@ -0,0 +1,8 @@
+#variaveis
+sandes = int(input("Indique a quantidade de sandes a fazer:\n"))
+#processamento
+queijo = ((2 * 50) * sandes)/1000
+presunto = (50 * sandes)/1000
+hamburguer = (100 * sandes)/1000
+#saida de dados
+print("Para",sandes,"sandes, será preciso",queijo,"kgs de queijo",presunto,"kgs de presunto e ",hamburguer,"kgs de hamburguer.")
