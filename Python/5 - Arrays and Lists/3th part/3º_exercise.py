ff22329e794f8cbd1e12417fab9649e6a8439a35
Simão Costa <simadinisvc@gmail.com>
2022-03-16 22:42:36 +0000

    Update 3º_exercise.py

--- Python/5 - Arrays and Lists/3th part/3º_exercise.py
+++ Python/5 - Arrays and Lists/3th part/3º_exercise.py
@@ -1,5 +1,14 @@
 L1 = list(map(int, input().split()))
 L2 = input().split()
 
-#for i in range():
-    
+res = L1 + L2 
+res[::2] = L1 
+res[1::2] = L2
+
+
+string = ""
+
+for i in range(len(res)):
+    string += str(res[i])
+
+print(string)
