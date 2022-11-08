f7a2261ed964e12bc8917d783e1683d25e57a10f
Simão Costa <simadinisvc@gmail.com>
2022-11-08 13:08:26 +0000

    SW Uploud 08/11/22

---
+++ C#/2 - OOP/Desafios de Aula/5º_exercise/Produto.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace _5º_exercise
+{
+    internal class Produto
+    {
+        // Atributos
+        private string nome;
+        double precoCusto, precoVenda, margemLucro;
+
+        public Produto() // Construtor Vazio para versões .NET obrigatórias
+        { }
+
+        public Produto(string nome, double precoCusto, double precoVenda) // Construtor Predefinição
+        {
+            this.nome = nome;
+            this.precoCusto = precoCusto;
+            this.precoVenda = precoVenda;
+        }
+        
+        // Encapsulamento
+
+        public string retornaNome()
+        {
+            return nome;
+        }
+
+        public double retornaCusto()
+        {
+            return precoCusto;
+        }
+
+        public double retornaVenda()
+        {
+            return precoVenda;
+        }
+
+        public double retornaMargemLucro()
+        {
+            return margemLucro;
+        }
+
+        public void alteraNome(string nome)
+        {
+            this.nome = nome;
+        }
+
+        public void alteraCusto(double precoCusto)
+        {
+            if (precoVenda >= precoCusto)
+                this.precoCusto = precoCusto;
+            else
+                Console.Error.WriteLine("Desculpe, mas o preco do Custo não pode ser superior ao preco de Venda.");
+        }
+
+        public void alteraVenda(double precoVenda)
+        {
+            if (precoVenda >= precoCusto)
+                this.precoVenda = precoVenda;
+            else
+                Console.Error.WriteLine("Desculpe, mas o preco do Venda não pode ser inferior ao preco de Custo.");
+        }
+
+        public double calcularMargemLucro()
+        {
+            margemLucro = precoVenda - precoCusto;
+            return margemLucro;
+        }
+
+        public double getMargemLucroPorcentagem()
+        {
+            return Math.Round(margemLucro * 100 / precoCusto, 1);
+        }
+    }
+}
---
+++ C#/2 - OOP/Desafios de Aula/5º_exercise/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _5º_exercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome;
+            double custo, venda;
+
+            Console.Write("Digite o nome: ");
+            nome = Console.ReadLine();
+            Console.Write("Digite o preço de custo: ");
+            bool booleanCusto = double.TryParse(Console.ReadLine(), out custo);
+            Console.Write("Digite o preço de venda: ");
+            bool booleanVenda = double.TryParse(Console.ReadLine(), out venda);
+
+            while (!booleanCusto) // Proteção
+            {
+                Console.Error.WriteLine("Tem de introduzir numeros.\nDigite o preço de custo: ");
+                booleanCusto = double.TryParse(Console.ReadLine(), out custo);
+            }
+
+            while (!booleanVenda) // Proteção
+            {
+                Console.WriteLine("Tem de introduzir numeros.\nDigite o preço de venda: ");
+                booleanVenda = double.TryParse(Console.ReadLine(), out custo);
+            }
+
+            Produto p1 = new Produto(nome, custo, venda); // Objeto
+
+            Console.WriteLine("Preço do custo: {0} euros", p1.retornaCusto());
+            Console.WriteLine("Preço do venda: {0} euros", p1.retornaVenda());
+            Console.WriteLine("Margem de Lucro: {0} euros", p1.calcularMargemLucro());
+            Console.WriteLine("Margem de Lucro em Percentagem: {0}%", p1.getMargemLucroPorcentagem());
+
+            Console.Write("Pressione qualquer tecla para sair...");
+            Console.ReadKey();
+        }
+    }
+}
