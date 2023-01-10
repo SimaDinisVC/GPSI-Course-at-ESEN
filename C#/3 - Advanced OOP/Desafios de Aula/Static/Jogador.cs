93cf0632cb58e09963ce8e01730d704dfb96a8a7
Simão Costa <simadinisvc@gmail.com>
2023-01-10 13:04:40 +0000

    SW Update 10/01/23

---
+++ C#/3 - Advanced OOP/Desafios de Aula/Static/Jogador.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Static
+{
+    internal static class Jogador
+    {
+        public static int energia;
+        public static bool vida;
+        public static string nome;
+
+        public static void IniciarJogador(string n)
+        {
+            energia = 100;
+            vida = true;
+            nome = n;
+        }
+
+        public static void info()
+        {
+            Console.WriteLine(energia);
+            Console.WriteLine(vida);
+            Console.WriteLine(nome);
+        }
+    }
+}
---
+++ C#/3 - Advanced OOP/Desafios de Aula/Static/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Static
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Jogador.info();
+        }
+    }
+}
